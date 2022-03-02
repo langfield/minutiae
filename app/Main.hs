63bbae9b70509714a454dcbd321945bf9c46bbd1
mal <mal@debian-BULLSEYE-live-builder-AMD64>
2022-03-02 17:24:23 +0000

    Figured out how to import gogol.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Lib
+import Example.Sheets
+
+import Options.Applicative
+import Data.Semigroup ((<>))
+
+data Sample = Sample
+  { hello      :: String
+  , quiet      :: Bool
+  , enthusiasm :: Int }
+
+sample :: Parser Sample
+sample = Sample
+      <$> strOption
+          ( long "hello"
+         <> metavar "TARGET"
+         <> help "Target for the greeting" )
+      <*> switch
+          ( long "quiet"
+         <> short 'q'
+         <> help "Whether to be quiet" )
+      <*> option auto
+          ( long "enthusiasm"
+         <> help "How enthusiastically to greet"
+         <> showDefault
+         <> value 1
+         <> metavar "INT" )
+
+{-
+main :: IO ()
+main = greet =<< execParser opts
+  where
+    opts = info (sample <**> helper)
+      ( fullDesc
+     <> progDesc "Print a greeting for TARGET"
+     <> header "hello - a test for optparse-applicative" )
+
+greet :: Sample -> IO ()
+greet (Sample h False n) = putStrLn $ "Hello, " ++ h ++ replicate n '!'
+greet _ = return ()
+ -}
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Downloading sheet."
+    ss <- exampleGetSheet "0"
+    putStrLn "Downloaded sheet."
