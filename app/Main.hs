94856fc808409e2c4e7081660b209c286ad3de82
mal <mal@debian-BULLSEYE-live-builder-AMD64>
2022-03-05 00:01:00 +0000

    All parsers working except for UTCTime.

--- app/Main.hs
+++ app/Main.hs
@@ -11,7 +11,11 @@ import Data.List
 import Data.Semigroup ((<>))
 import Data.Text
 import Data.Time
+import Data.Aeson
+import Data.Aeson.Types
+import Data.ByteString.Lazy.UTF8
 import Data.Typeable
+import Data.Scientific
 
 import Control.Lens
 
@@ -29,7 +33,7 @@ newtype Args =
     { id :: String
     }
 
-sample :: Parser Args
+sample :: Options.Applicative.Parser Args
 sample =
   Args <$>
   strOption
@@ -47,27 +51,46 @@ main = demo =<< execParser opts
          progDesc "Download a Google sheet." <>
          header "diurnal - a scheduling system.")
 
+-- Convert from JSONValue to String.
+jsonValueToString :: Value -> Maybe String
+jsonValueToString (String s) = Just (unpack s)
+jsonValueToString _ = Nothing
+
+jsonStringValueToInt :: Value -> Maybe Int
+jsonStringValueToInt (String s) = Just (read (unpack s) :: Int)
+jsonStringValueToInt _ = Nothing
+
+jsonStringValueToFloat :: Value -> Maybe Float
+jsonStringValueToFloat (String s) = Just (read (unpack s) :: Float)
+jsonStringValueToFloat _ = Nothing
+
+jsonValueToUTCTime :: Value -> Maybe UTCTime
+jsonValueToUTCTime (String s) = Just (read (unpack s) :: UTCTime)
+jsonValueToUTCTime _ = Nothing
+
 data Block =
   Block
-    { title :: String
-    , completion :: Float
-    , weight :: Float
-    , actualMins :: Int
-    , mins :: Int
+    { title :: Maybe String
+    , completion :: Maybe Float
+    , weight :: Maybe Float
+    , actualMins :: Maybe Int
+    , mins :: Maybe Int
+    , late :: Maybe Int
     , time :: UTCTime
     }
-  deriving (Data, Typeable)
+  deriving (Data, Typeable, Show)
 
 parseBlock :: [JSONValue] -> Block
 parseBlock blockList
-  | Data.List.length blockList /= 6 = error "Bad."
+  | Data.List.length blockList /= 7 = error "Number of columns in block is wrong!"
   | otherwise =
     Block
-      { title = parseJSON (blockList !! 0) :: String
-      , completion = 0.5
-      , weight = 0.5
-      , actualMins = 1
-      , mins = 1
+      { title = jsonValueToString (blockList !! 0) :: Maybe String
+      , completion = jsonStringValueToFloat (blockList !! 1) :: Maybe Float
+      , weight = jsonStringValueToFloat (blockList !! 2) :: Maybe Float
+      , actualMins = jsonStringValueToInt (blockList !! 3) :: Maybe Int
+      , mins = jsonStringValueToInt (blockList !! 4) :: Maybe Int
+      , late = jsonStringValueToInt (blockList !! 5) :: Maybe Int
       , time = UTCTime (fromGregorian 2021 03 05) (secondsToDiffTime 0)
       }
 
@@ -82,12 +105,16 @@ demo (Args id) = do
   let jsonValueArray :: [[JSONValue]] = valueRange ^. vrValues
   print (typeOf jsonValueArray)
   let row = jsonValueArray !! 0
+  putStrLn ("Length of row:" ++ show (Data.List.length row))
   let block = parseBlock row
+  {-
   let bLen =
         Prelude.map constrFields . dataTypeConstrs . dataTypeOf $
         (undefined :: Block)
-  print (typeOf bLen)
+  -}
+  -- print (typeOf bLen)
   pPrint row
   print (typeOf row)
+  pPrint block
 -- If we don't match on first pattern, we simply return.
 demo _ = return ()
